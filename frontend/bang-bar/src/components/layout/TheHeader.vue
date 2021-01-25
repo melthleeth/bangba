c5e200cc92d7bbb112663c4ff4651747bd35a1b1
이혜진 <hichjay@naver.com>
2021-01-25 02:33:13 +0000

    Frontend branch merge and delete 요청

---
+++ frontend/bang-bar/src/components/layout/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <span>
+            <img src="../../assets/img/logo.png"> 방구석 바텐더
+        </span>
+    </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    body {
+       position: relative;
+    width: 1440px;
+    height: 120px;
+
+    background: #FFFFFF;
+    border: 1px solid #000000;
+    box-sizing: border-box;
+    }
+
+    img {
+        float: auto;
+    }
+</style>
