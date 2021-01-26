af31a62637676dcfde3cd5c222b59586b0d3037d
Duckin_Jang <77999jno@kookmin.ac.kr>
2021-01-26 15:03:54 +0900

    S04P12A401-130 [+] Edit AWS 연동 수정 & Swagger로 회원가입 처리

---
+++ backend/src/main/java/com/bangba/project730/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.bangba.project730;
+
+import javax.swing.text.Document;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	// Swagger-UI 확인 
+	// http://localhost:8080/swagger-ui.html#/
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+}
