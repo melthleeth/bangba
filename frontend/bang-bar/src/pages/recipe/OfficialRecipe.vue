43a65947123d8496a3f21b6a2233be9ab3234523
Hyejin Lee <hichjay@gmail.com>
2021-01-28 13:10:59 +0900

    S04P12A401-141 [+] 게시글 리스트 페이지 구현
    레시피 카드 component 형식으로 grid layout으로 뿌리기

--- frontend/bang-bar/src/pages/recipe/OfficialRecipe.vue
+++ frontend/bang-bar/src/pages/recipe/OfficialRecipe.vue
@@ -1,18 +1,48 @@
 <template>
-  <p>오피셜 레시피</p>
+  <div class="flex flex-col justify-items-center">
+    <span class="title text-center my-10">오피셜 레시피</span>
+    <div class=" grid grid-cols-4 grid-flow-row gap-4">
+      <recipe-card
+        v-for="cocktail in cocktails"
+        :key="cocktail.no"
+        :cocktailname="cocktail.cocktailname"
+        :tag="cocktail.tag"
+        :username="cocktail.username"
+        :like="cocktail.like"
+        :bookmared="cocktail.bookmared"
+      >
+      </recipe-card>
+    </div>
+  </div>
 </template>
 
+<script>
+import recipe from "../../data/recipe.js";
+import RecipeCard from '../../components/recipes/RecipeCard.vue';
+export default {
+  components: {
+    RecipeCard,
+  },
+  data() {
+    return {
+      cocktails: recipe.data,
+    };
+  },
+};
+</script>
+
 <style scoped>
-p {
-  margin: 0;
+.title {
   font-family: Noto Sans KR;
   font-style: normal;
   font-weight: bold;
   font-size: 64px;
   line-height: 93px;
-  display: flex;
-  align-items: center;
 
   color: #23232f;
+}
+
+.card-corner {
+  border-radius: 30px;
 }
 </style>
