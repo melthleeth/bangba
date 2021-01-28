ad5bfadd2de24d8fc22e186a38a812cfd6a860ef
Duckin_Jang <77999jno@kookmin.ac.kr>
2021-01-28 23:04:48 +0900

    S04P12A401-184 [+] Add 회원, 관리자 기능 테스트 & rest api 수정

--- backend/src/main/java/com/bangba/project730/controller/ArticleController.java
+++ backend/src/main/java/com/bangba/project730/controller/ArticleController.java
@@ -8,7 +8,10 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,7 +49,7 @@ public class ArticleController {
 
 	@ApiOperation(value = "레시피 검색", response = String.class)
 	@ApiImplicitParams({})
-	@PostMapping("/search")
+	@GetMapping("/keyword")
 	public String searchArticle(@RequestBody List<String> tags, Model model) throws Exception {
 		try {
 			articleService.searchArticle(tags);
@@ -60,7 +63,7 @@ public class ArticleController {
 	}
 	@ApiOperation(value = "레시피 수정", response = String.class)
 	@ApiImplicitParams({})
-	@PostMapping("/update")
+	@PutMapping("/{pk_article}")
 	public String updateArticle(@RequestBody Map<String, String> map, Model model) throws Exception {
 		try {
 			articleService.updateArticle(map);
@@ -74,7 +77,7 @@ public class ArticleController {
 	}
 	@ApiOperation(value = "레시피 삭제", response = String.class)
 	@ApiImplicitParams({})
-	@PostMapping("/delete")
+	@DeleteMapping("/{pk_article}")
 	public String deleteArticle(@RequestParam Integer pk_article, Model model) throws Exception {
 		try {
 			articleService.deleteArticle(pk_article);
@@ -88,7 +91,7 @@ public class ArticleController {
 	}
 	@ApiOperation(value = "재료 추가", response = String.class)
 	@ApiImplicitParams({})
-	@PostMapping("/addingredient")
+	@PostMapping("/ingredient")
 	public String createIngredient(@RequestParam String ingredient, Model model) throws Exception {
 		try {
 			articleService.createIngredient(ingredient);
@@ -104,7 +107,7 @@ public class ArticleController {
 	}
 	@ApiOperation(value = "태그 추가", response = String.class)
 	@ApiImplicitParams({})
-	@PostMapping("/addtag")
+	@PostMapping("/tag")
 	public String createTag(@RequestParam String tag, Model model) throws Exception {
 		try {
 			articleService.createTag(tag,3);
@@ -115,47 +118,44 @@ public class ArticleController {
 			model.addAttribute("msg", "태그 추가중 문제가 발생했습니다.");
 			return "error";
 		}
-	}
-
-
+	}	
 	
-	
-	 @PostMapping("/upload")
-	    public String upload(@RequestParam("file") MultipartFile file) {
-	 
-	        System.out.println("파일 이름 : " + file.getOriginalFilename());
-	        System.out.println("파일 크기 : " + file.getSize());
-	        // 데이터 베이스에 정보를 저장하는 건
-	        // 이렇게 file의 get 메소드를 활용해 필요한 정보들을 가져오고 
-	        // 그걸 DTO에 담아 insert하면 된다. 
-	        // 간단한거니 후의 과정은 생략하고 파일로 서버에 저장하는 걸 보자면
-	 
-	        try (
-	                // 맥일 경우
-	                // FileOutputStream fos = new FileOutputStream("/tmp/" +file.getOriginalFilename());
-	                // 윈도우일 경우
-	                FileOutputStream fos = new FileOutputStream("C:/Users/Jin/Downloads/USER_SIGNUP, LOGIN, LOGOUT/backend/imgs/" + file.getOriginalFilename());
-	                // 파일 저장할 경로 + 파일명을 파라미터로 넣고 fileOutputStream 객체 생성하고
-	                InputStream is = file.getInputStream();) {
-	                // file로 부터 inputStream을 가져온다.
-	        	
-		        System.out.println("파일 경로 : C:/Users/Jin/Downloads/USER_SIGNUP, LOGIN, LOGOUT/backend/imgs/" + file.getOriginalFilename());
-	            int readCount = 0;
-	            byte[] buffer = new byte[1024];
-	            // 파일을 읽을 크기 만큼의 buffer를 생성하고
-	            // ( 보통 1024, 2048, 4096, 8192 와 같이 배수 형식으로 버퍼의 크기를 잡는 것이 일반적이다.)
-	            
-	            while ((readCount = is.read(buffer)) != -1) {
-	                //  파일에서 가져온 fileInputStream을 설정한 크기 (1024byte) 만큼 읽고
-	                
-	                fos.write(buffer, 0, readCount);
-	                // 위에서 생성한 fileOutputStream 객체에 출력하기를 반복한다
-	            }
-	        } catch (Exception ex) {
-	            throw new RuntimeException("file Save Error");
-	        }
-	 
-	        return "uploadok";
-	    }
+	 @PostMapping("/photo")
+    public String upload(@RequestParam("file") MultipartFile file) {
+ 
+        System.out.println("파일 이름 : " + file.getOriginalFilename());
+        System.out.println("파일 크기 : " + file.getSize());
+        // 데이터 베이스에 정보를 저장하는 건
+        // 이렇게 file의 get 메소드를 활용해 필요한 정보들을 가져오고 
+        // 그걸 DTO에 담아 insert하면 된다. 
+        // 간단한거니 후의 과정은 생략하고 파일로 서버에 저장하는 걸 보자면
+ 
+        try (
+                // 맥일 경우
+                // FileOutputStream fos = new FileOutputStream("/tmp/" +file.getOriginalFilename());
+                // 윈도우일 경우
+                FileOutputStream fos = new FileOutputStream("C:/Users/Jin/Downloads/USER_SIGNUP, LOGIN, LOGOUT/backend/imgs/" + file.getOriginalFilename());
+                // 파일 저장할 경로 + 파일명을 파라미터로 넣고 fileOutputStream 객체 생성하고
+                InputStream is = file.getInputStream();) {
+                // file로 부터 inputStream을 가져온다.
+        	
+	        System.out.println("파일 경로 : C:/Users/Jin/Downloads/USER_SIGNUP, LOGIN, LOGOUT/backend/imgs/" + file.getOriginalFilename());
+            int readCount = 0;
+            byte[] buffer = new byte[1024];
+            // 파일을 읽을 크기 만큼의 buffer를 생성하고
+            // ( 보통 1024, 2048, 4096, 8192 와 같이 배수 형식으로 버퍼의 크기를 잡는 것이 일반적이다.)
+            
+            while ((readCount = is.read(buffer)) != -1) {
+                //  파일에서 가져온 fileInputStream을 설정한 크기 (1024byte) 만큼 읽고
+                
+                fos.write(buffer, 0, readCount);
+                // 위에서 생성한 fileOutputStream 객체에 출력하기를 반복한다
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("file Save Error");
+        }
+ 
+        return "uploadok";
+    }
 
 }
--- backend/src/main/java/com/bangba/project730/controller/UserController.java
+++ backend/src/main/java/com/bangba/project730/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.bangba.project730.controller;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -18,22 +20,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bangba.project730.model.dto.UserDto;
 import com.bangba.project730.model.service.UserService;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserService userService;
-	
+
 	@ApiOperation(value = "회원가입 실행", response = String.class)
-	@PostMapping("/signup")
-	@ResponseBody public String createUser(UserDto userDto, Model model) throws Exception {
-		if(userService.isDuplicatedEmail(userDto.getEmail()) != 0 || userService.isDuplicatedName(userDto.getUser_name()) != 0) {
+	@PostMapping("/join")
+	@ResponseBody
+	public String createUser(@RequestBody @ApiParam(value = "회원 한 명의 정보를 담는 객체", required = true) UserDto userDto,
+			Model model) throws Exception {
+		if (userService.isDuplicatedEmail(userDto.getEmail()) != 0
+				|| userService.isDuplicatedName(userDto.getUser_name()) != 0) {
 			return "FAIL";
 		} else {
 			try {
@@ -47,40 +54,45 @@ public class UserController {
 			}
 		}
 	}
-	
+
 	@ApiOperation(value = "로그인", response = String.class)
-	@PostMapping(value="/login")
-	@ResponseBody public String login(@RequestParam Map<String, String> map, Model model, HttpSession session) {
+	@PostMapping(value = "/login")
+	@ResponseBody
+	public String login(@RequestBody @ApiParam(value = "로그인 정보를 담는 객체", required = true) Map<String, String> map,
+			Model model, HttpSession session) {
 		try {
 			UserDto user = userService.login(map);
-			if(user != null) {
+			if (user != null) {
 				session.setAttribute("userinfo", user);
 				return "SUCCESS";
 			} else {
 				model.addAttribute("msg", "아이디 또는 비밀번호를 확인 후 로그인해 주세요.");
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			model.addAttribute("msg", "로그인 중 문제가 발생했습니다.");
 			return "error/error";
 		}
 		return "FAIL";
 	}
-	
+
 	@ApiOperation(value = "로그아웃", response = String.class)
-	@GetMapping(value="/logout")
-	@ResponseBody public String logout(HttpSession session) {
+	@GetMapping(value = "/logout")
+	@ResponseBody
+	public String logout(HttpSession session) {
 		session.invalidate();
 		return "SUCCESS";
 	}
-	
+
 	@ApiOperation(value = "이메일 중복 확인 및 인증", response = String.class)
-	@PostMapping(value="/mail")
-	@ResponseBody public String messageSend(@RequestBody String toAddress, Model model) {
+	@PostMapping(value = "/join/mail")
+	@ResponseBody
+	public String messageSend(@RequestBody @ApiParam(value = "회원가입에 필요한 이메일", required = true) String toAddress,
+			Model model) {
 		int result = userService.isDuplicatedEmail(toAddress);
-		if(result != 0) { // 이미 이메일이 존재할 때
+		if (result != 0) { // 이미 이메일이 존재할 때
 			model.addAttribute("msg", "증복되는 이메일입니다.");
-		} else { // 이메일이 존재하지 않을 때 
+		} else { // 이메일이 존재하지 않을 때
 			try {
 				String code = makeCode();
 				String codeMsg = "인증코드는 " + code + " 입니다.";
@@ -88,78 +100,118 @@ public class UserController {
 				model.addAttribute("authCode", code);
 				model.addAttribute("msg", "인증번호가 발송되었습니다.");
 				return "SUCCESS";
-			} catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 				model.addAttribute("msg", "이메일 인증에 실패했습니다.");
 			}
 		}
 		return "FAIL";
 	}
-	
+
 	@ApiOperation(value = "닉네임 중복 확인", response = String.class)
-	@PostMapping(value="/name")
-	@ResponseBody public String confirmName(@RequestBody String user_name, Model model) {
+	@PostMapping(value = "/join/name")
+	@ResponseBody
+	public String confirmName(@RequestBody @ApiParam(value = "회원가입에 필요한 닉네임", required = true) String user_name,
+			Model model) {
 		int result = userService.isDuplicatedName(user_name);
-		
-		if(result != 0) { //이미 사용중인 닉네임일 경우 
-			model.addAttribute("msg","이미 사용중인 닉네임입니다.");
+
+		if (result != 0) { // 이미 사용중인 닉네임일 경우
+			model.addAttribute("msg", "이미 사용중인 닉네임입니다.");
 			return "FAIL";
 		} else {
 			model.addAttribute("msg", "사용가능한 닉네임입니다.");
 			return "SUCCESS";
 		}
-	}	
-	
+	}
+
 	@ApiOperation(value = "마이페이지 수정 - 먼저 패스워드만 가능", response = String.class)
-	@PutMapping(value="/update")
-	@ResponseBody public UserDto updateMyPage(UserDto userDto) {
+	@PutMapping(value = "/mypage/options/pw")
+	@ResponseBody
+	public UserDto updateMyPage(@RequestBody @ApiParam(value = "회원 한 명의 정보를 담는 객체", required = true) UserDto userDto) {
 		userService.updateMyPage(userDto);
 		return userService.getMyPage(userDto.getPk_user());
 	}
-	
+
 	@ApiOperation(value = "회원 탈퇴", response = String.class)
-	@DeleteMapping(value="/delete")
-	@ResponseBody public String deleteMyPage(int pk_user, Model model) {
+	@DeleteMapping(value = "/mypage/{pk_user}")
+	@ResponseBody
+	public String deleteMyPage(@ApiParam(value = "유저의 pk", required = true) int pk_user, Model model) {
 		userService.deleteMyPage(pk_user);
 		return "SUCCESS";
 	}
-	
+
 	@ApiOperation(value = "비밀번호찾기", response = String.class)
-	@PostMapping(value="/findpw")
-	@ResponseBody public String findPassword(String toAddress, Model model) {
+	@PostMapping(value = "/help")
+	@ResponseBody
+	public String findPassword(@ApiParam(value = "비밀번호를 찾을 해당 계정의 이메일", required = true) String toAddress,
+			Model model) {
 		int result = userService.isDuplicatedEmail(toAddress);
-		if(result == 0) { // 이메일이 존재하지 않을 때 
+		if (result == 0) { // 이메일이 존재하지 않을 때
 			model.addAttribute("msg", "회원가입이 되어있지 않은 이메일입니다.");
 		} else { // 회원가입된 이메일일 경우
 			try {
 				String code = makeCode();
-				String codeMsg = "인증코드는 " + code + " 입니다.";
+				String codeMsg = "인증코드는 [" + code + "] 입니다.";
 				userService.sendEmail(toAddress, "방바! 이메일 인증 발송 코드입니다.", codeMsg);
 				model.addAttribute("authCode", code);
 				model.addAttribute("msg", "인증번호가 발송되었습니다.");
 				return code;
-			} catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 				model.addAttribute("msg", "이메일 인증에 실패했습니다.");
 			}
 		}
 		return "FAIL";
 	}
-	
+
+	@ApiOperation(value = "유저 프로필 저장 - 해당 파일이 저장된 위치를 반환", response = String.class)
+	@PostMapping("/mypage/photo")
+    @ResponseBody public String upload(@RequestParam("file") MultipartFile file) {
+ 
+        System.out.println("파일 이름 : " + file.getOriginalFilename());
+        System.out.println("파일 크기 : " + file.getSize());
+ 
+        String fileRoot = "/home/ubuntu/imgs/user/" + file.getOriginalFilename();
+    	
+        try (	
+            // 윈도우일 경우
+            FileOutputStream fos = new FileOutputStream(fileRoot);
+            // 파일 저장할 경로 + 파일명을 파라미터로 넣고 fileOutputStream 객체 생성하고
+            InputStream is = file.getInputStream();) {
+            // file로 부터 inputStream을 가져온다.
+    	
+	        System.out.println("/home/ubuntu/imgs/user/" + file.getOriginalFilename());
+            int readCount = 0;
+            byte[] buffer = new byte[1024];
+            // 파일을 읽을 크기 만큼의 buffer를 생성하고
+            // ( 보통 1024, 2048, 4096, 8192 와 같이 배수 형식으로 버퍼의 크기를 잡는 것이 일반적이다.)
+            
+            while ((readCount = is.read(buffer)) != -1) {
+                //  파일에서 가져온 fileInputStream을 설정한 크기 (1024byte) 만큼 읽고
+                
+                fos.write(buffer, 0, readCount);
+                // 위에서 생성한 fileOutputStream 객체에 출력하기를 반복한다
+            }
+            return fileRoot;
+        } catch (Exception e) {
+            throw new RuntimeException("file Save Error");
+        }
+    }
+
 //  인증 코드 생성 
 	public String makeCode() {
 		String temp = "";
 		Random rnd = new Random();
-		for(int i = 0; i < 12; i++) {
+		for (int i = 0; i < 12; i++) {
 			int rIndex = rnd.nextInt(3);
-			switch(rIndex) {
+			switch (rIndex) {
 			case 0:
 				temp += ((char) ((int) (rnd.nextInt(26)) + 97));
 				break;
 			case 1:
 				temp += ((char) ((int) (rnd.nextInt(26)) + 65));
 				break;
-			case 2: 
+			case 2:
 				temp += ((rnd.nextInt(10)));
 				break;
 			}
