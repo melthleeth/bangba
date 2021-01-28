f5b626d0d9e27c323fcfa648bb1a65523c409a5c
Hyejin Lee <hichjay@gmail.com>
2021-01-29 01:59:06 +0900

    S04P12A401-186 [+] 로고 및 폰트 수정

--- frontend/bang-bar/src/components/layout/TheFooter.vue
+++ frontend/bang-bar/src/components/layout/TheFooter.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
   <footer>
     <div class="container">
         <router-link to="/">
@@ -6,9 +7,27 @@
           <img src="../../assets/img/logo.png" />
           <span>방구석 바텐더</span>
       </section>
+<<<<<<< Updated upstream
         </router-link>
+=======
+=======
+  <footer class="my-10">
+    <div class="flex flex-col justify-items-center">
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
       <section>
-        <nav>
+      <router-link to="/">
+        <article class="logo flex flex-col justify-items-center w-max">
+          <img src="../../assets/img/logo.png" alt="logo-image" />
+          <span
+            class="text-xl text-center font-color-black-400 font-Mapo-DPPA mt-2"
+            >방구석 바텐더</span
+          >
+        </article>
+      </router-link>
+      </section>
+      <section class="">
+        <nav class="font-color-black-400 text-lg">
           <ul>
             <li><router-link to="/footer/guide">방바 가이드</router-link></li>
             <li><router-link to="/footer/people">방바 사람들</router-link></li>
@@ -16,8 +35,10 @@
           </ul>
         </nav>
       </section>
-      <section>
-        <p>2021 ⓒ 방구석 바텐더 All rights reserved</p>
+      <section class="my-6">
+        <span class="font-S-CoreDream-light"
+          >2021 ⓒ 방구석 바텐더 All rights reserved</span
+        >
       </section>
     </div>
   </footer>
@@ -32,26 +53,45 @@ export default {};
   text-align: center;
 }
 
+nav {
+  font-family: "S-CoreDream-5Medium";
+}
+
 section {
   align-items: center;
 }
 
+img {
+  margin: auto;
+}
+
 .logo {
+<<<<<<< Updated upstream
     font-size: 15pt;
     border-radius: 50%;
     margin: 1rem;
     padding: 0.5rem;
+<<<<<<< Updated upstream
     display: flex;
     transition: .3s ease-out;
+=======
+=======
+  font-size: 15pt;
+  border-radius: 50%;
+  margin: 1rem;
+  padding: 0.5rem;
+  transition: 0.3s ease-out;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
 
 .logo p {
-    margin: 0;
+  margin: 0;
 }
 
 .logo:hover {
-    /* border-bottom: 3px solid #FF5E46; */
-    background: #FF5E46;
+  /* border-bottom: 3px solid #FF5E46; */
+  background: #ff5e46;
 }
 
 nav {
@@ -61,18 +101,15 @@ nav {
 footer {
   width: 100%;
   height: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 
 footer a {
   text-decoration: none;
   color: #23232f;
   display: inline-block;
-  padding: 0.75rem 1.5rem;
+  padding: 0.25rem 1.5rem;
   border: 1px solid transparent;
-  transition: .3s ease-out;
+  transition: 0.3s ease-out;
 }
 
 a:active,
@@ -81,7 +118,7 @@ a.router-link-active {
 }
 
 li a {
-    border-radius: 50px;
+  border-radius: 50px;
 }
 
 li a:hover,
@@ -95,9 +132,6 @@ li a.router-link-active {
 footer nav {
   width: 90%;
   margin: auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 }
 
 footer ul {
@@ -111,7 +145,6 @@ footer ul {
 
 li {
   margin: 0 0.5rem;
-  width: 140px;
+  /* width: 140px; */
 }
-
 </style>
