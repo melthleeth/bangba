7dbdfbbb192915b722f0b9635d9d724a682a7a08
황경서 <kyungseo55@gmail.com>
2021-02-01 14:25:07 +0900

    S04P13A401-14 [+] Edit forum dao 생성

--- backend/src/main/java/com/bangba/project730/model/dao/ForumDao.java
+++ backend/src/main/java/com/bangba/project730/model/dao/ForumDao.java
@@ -5,18 +5,16 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.bangba.project730.model.dto.AlcoholDto;
-import com.bangba.project730.model.dto.ArticleDto;
-import com.bangba.project730.model.dto.CupDto;
 import com.bangba.project730.model.dto.ForumDto;
-import com.bangba.project730.model.dto.IngredientDto;
-import com.bangba.project730.model.dto.TagDto;
+
 
 @Mapper
 public interface ForumDao {
 	public void createForum(ForumDto forumdto) throws Exception;
-	public ForumDto searchForum(List<String> pk_forum) throws Exception;
+	public List<ForumDto> listAll() throws Exception;
+	public ForumDto read(int pk_forum) throws Exception;
 	public void updateForum(Map<String, String> map) throws Exception;
+	//public void updateForum(int pk_forum) throws Exception;
 	public void deleteForum(int pk_forum) throws Exception;
 
 	// public void addImage(int pk_forum, int pk_fimg) throws Exception;
