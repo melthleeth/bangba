88a2bf0ef99fbbfa72cf8f885f6f6749d6b56297
Jin <Jin@218.154.7.105>
2021-02-01 15:53:16 +0900

    sry commit

--- backend/src/main/java/com/bangba/project730/model/service/ArticleService.java
+++ backend/src/main/java/com/bangba/project730/model/service/ArticleService.java
@@ -1,13 +1,12 @@
 package com.bangba.project730.model.service;
 
-import java.util.List;
 import java.util.Map;
 
 import com.bangba.project730.model.dto.ArticleDto;
 
 public interface ArticleService {
 	public void createArticle(Map<String, String> map) throws Exception;
-	public ArticleDto searchArticle(List<String> tags) throws Exception;
+	public ArticleDto searchArticle(Map<String, String> map) throws Exception;
 	public void updateArticle(Map<String, String> map) throws Exception;
 	public void deleteArticle(int pk_article) throws Exception;
 	
