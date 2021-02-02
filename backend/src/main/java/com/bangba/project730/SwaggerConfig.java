53879d7bfb9f01839cfa6b9d7312e5bb17f8c9fd
Duckin_Jang <77999jno@kookmin.ac.kr>
2021-02-02 10:19:55 +0900

    S04P13A401-2[+] 팔로우 CRUD 테스트

--- backend/src/main/java/com/bangba/project730/SwaggerConfig.java
+++ backend/src/main/java/com/bangba/project730/SwaggerConfig.java
@@ -15,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 	
 	// Swagger-UI 확인 
-	// http://localhost:8080/swagger-ui.html#/
+	// http://localhost:8081/swagger-ui.html#/
 	
 	@Bean
 	public Docket api() {
