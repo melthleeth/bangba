855bef698f837097448b383d32d66f4e31aba0f1
Jin <Jin@218.154.7.105>
2021-02-08 11:01:34 +0900

    backend 11:00

--- backend/src/main/java/com/bangba/project730/controller/ArticleController.java
+++ backend/src/main/java/com/bangba/project730/controller/ArticleController.java
@@ -7,11 +7,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,9 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.bangba.project730.model.dto.AlcoholDto;
 import com.bangba.project730.model.dto.ArticleDto;
-import com.bangba.project730.model.dto.Article_alcoholDto;
 import com.bangba.project730.model.dto.IngredientDto;
-import com.bangba.project730.model.dto.RecipeDto;
 import com.bangba.project730.model.dto.TagDto;
 import com.bangba.project730.model.service.ArticleService;
 
