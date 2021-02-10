09c3e8cb75796f356c992da0bae0a8eeded63434
Jin <Jin@218.154.7.105>
2021-02-10 13:11:51 +0900

    ForumCtr txt 수정

--- backend/src/main/java/com/bangba/project730/controller/ForumController.java
+++ backend/src/main/java/com/bangba/project730/controller/ForumController.java
@@ -127,7 +127,7 @@ public class ForumController {
 	}
 
 	@ApiOperation(value = "댓글 검색", response = String.class)
-	@PostMapping("/comment/keyword")
+	@PostMapping("/comment/{pk_forum}")
 	public List<FFcommentDto> searchComment(@RequestBody int pk_forum , Model model) throws Exception {
 		try {
 			model.addAttribute("msg", "댓글 검색 완료");
@@ -140,7 +140,7 @@ public class ForumController {
 	}
 	
 	@ApiOperation(value = "댓글 수정", response = String.class)
-	@PutMapping("/comment/{pk_article}")
+	@PutMapping("/comment/{pk_fcomment}")
 	public String updateComment(@RequestBody Map<String, String> map, Model model) throws Exception {
 		try {
 			forumService.updateComment(map);
@@ -153,7 +153,7 @@ public class ForumController {
 		}
 	}
 	@ApiOperation(value = "댓글 삭제", response = String.class)
-	@DeleteMapping("/comment/{pk_article}")
+	@DeleteMapping("/comment/delete/")
 	public String deleteComment(@RequestParam Integer pk_fcomment, Model model) throws Exception {
 		try {
 			forumService.deleteComment(pk_fcomment);
