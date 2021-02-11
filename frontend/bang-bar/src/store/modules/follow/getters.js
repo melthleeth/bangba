445d802721f94f7fb6ea68df1f46d8c368de0109
Duckin_Jang <77999jno@kookmin.ac.kr>
2021-02-11 11:17:14 +0900

    S04P13A401-68[+] Add 좋아요 북마크 테스트 & 포럼 댓글 에러 수정

---
+++ frontend/bang-bar/src/store/modules/follow/getters.js
@@ -0,0 +1,11 @@
+export default {
+    isFollow(state) {
+        return state.isFollow;
+    },
+    followCnt(state) {
+        return state.followCnt;
+    },
+    followingCnt(state) {
+        return state.followingCnt;
+    }
+};
