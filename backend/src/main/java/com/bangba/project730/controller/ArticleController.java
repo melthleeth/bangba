0030b83b23b471c064abcd52abcd9a3c24a43bb3
황경서 <kyungseo55@gmail.com>
2021-02-15 17:14:54 +0900

    S04P13A401-94 [+] 추천 술 정보 보내기

--- backend/src/main/java/com/bangba/project730/controller/ArticleController.java
+++ backend/src/main/java/com/bangba/project730/controller/ArticleController.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -421,4 +422,11 @@ public class ArticleController {
 		}
 		return -1;
 	}
+	
+	@ApiOperation(value = "베이스 술 정보 불러오기")
+    @GetMapping("/base-alcohol")
+    public List<AlcoholDto> searchBaseAlcohol() throws Exception {
+        return articleService.searchBaseAlcohol();
+    }
+	
 }
