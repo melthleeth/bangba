5de0e05e89c4a20e2567ff507a0b6106aeeec5db
황경서 <kyungseo55@gmail.com>
2021-02-15 17:15:14 +0900

    술 정보 프런트 연결

---
+++ frontend/bang-bar/src/components/users/UserBoard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="font-color-black-400 font-S-CoreDream-light text-center">
+    <span>남이 보는 프로필</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {},
+};
+</script>
+
+<style scoped></style>
