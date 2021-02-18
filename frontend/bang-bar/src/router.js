e5f84b1896579dbb56d04f27ef2466fc61259d60
Hyejin Lee <hichjay@gmail.com>
2021-02-18 16:02:02 +0900

    S04P13A401-56 [+] NotFound 페이지 개발
    임의 url 주소를 넣으면 안내하는 페이지 개발

--- frontend/bang-bar/src/router.js
+++ frontend/bang-bar/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./pages/Home.vue";
+import NotFound from "./pages/NotFound.vue";
 
 // account
 import SignUp from "./pages/account/SignUp.vue";
@@ -124,6 +125,8 @@ const router = createRouter({
 
     // mypage-other
     { path: "/mypageother/:nickname", component: MyPageOther },
+    // not found
+    { path: '/:notFound(.*)', component: NotFound }
   ],
 });
 
