b871512724298d4a23503d4cac477d643ba36139
Hyejin Lee <hichjay@gmail.com>
2021-02-18 16:08:15 +0900

    Footer 페이지 수정
    방바사람들, 문의, 가이드 페이지 수정

--- frontend/bang-bar/src/pages/footers/BangbaAsk.vue
+++ frontend/bang-bar/src/pages/footers/BangbaAsk.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="font-S-CoreDream-light font-color-black-400 py-12">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="lg:text-center">
+      <div class="text-center">
         <h2 class="text-base tracking-wide uppercase font-extrabold">
-          Contact bangba
+          Contact Bangba
         </h2>
-        <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <p
+          class="font-Mapo-DPPA mt-2 text-3xl leading-8 text-gray-900 sm:text-4xl leading-relaxed"
+        >
           문의
         </p>
-        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-          이곳은 방구석 사람들에게 문의를 보내는 곳입니다. 아래 input 값이 있지만 보내지는 않습니다.
+        <p
+          class="mt-4 max-w-2xl text-lg font-color-black-200 lg:mx-auto tracking-wide leading-relaxed"
+        >
+          방구석 바텐더 웹사이트에 대해 궁금하신 모든 사항은 환영입니다!
         </p>
       </div>
 
       <div class="mt-20">
-        <form class="w-full mx-auto max-w-3xl bg-white shadow p-8 text-gray-700 card">
+        <form
+          class="w-full mx-auto max-w-3xl bg-white shadow p-8 text-gray-700 card"
+        >
           <div class="flex flex-wrap mb-6">
             <div class="relative w-full appearance-none label-floating">
               <input
@@ -69,10 +75,10 @@
 
           <div class="">
             <button
-              class="w-full shadow bg-gray-700 hover:bg-gray-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              class="w-full shadow bg-gray-700 hover:bg-gray-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded-full"
               type="submit"
             >
-              Send
+              보내기
             </button>
           </div>
         </form>
@@ -82,9 +88,6 @@
 </template>
 
 <style scoped>
-body {
-  font-family: Noto Sans KR;
-}
 
 .card {
   border-radius: 30px;
